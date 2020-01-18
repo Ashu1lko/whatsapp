a38b58e7464dcf933a24ebc0ce0f0900622630c4
Ashu1lko <shuklaashutosh955@gmail.com>
2020-01-18 18:47:16 +0530

    second

---
+++ lib/status.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class status extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Status'),
+      )
+      
+    );
+  }
+}
